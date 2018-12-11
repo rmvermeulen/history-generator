6b02ad8541dee386886f6218b8b7af5a0e7a8c76
Rasmus Vermeulen <vermeulen.rasmus@gmail.com>
2018-12-11 14:26:23 +0100

    Lint and config changes

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,10 @@
 module.exports = {
   'package.json': ['prettier-package-json --write', 'git add'],
-  'src/**/*.{ts,json}': [
-    'prettier --write',
+  '{src,__tests__}/**/*.{ts,json}': [
+    'yarn format',
     'git add',
-    'jest --findRelatedTests'
-  ]
+    'jest --findRelatedTests',
+  ],
+  'src/**/*.ts': ['yarn lint --fix', 'git add'],
+  '__tests__/**/*.ts': ['yarn lint:test --fix', 'git add'],
 }
